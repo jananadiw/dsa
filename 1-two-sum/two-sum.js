325a8279e5f460941a68a9b8f1cc0e77c7cfbfab
Jananadi Wedagedara <jananadi92@gmail.com>
2024-11-07 01:34:37 +0100

    Time: 0 ms (100.00%) | Memory: 51.8 MB (16.44%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,25 +4,24 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    // 2 + 7 = 9 target, return nums[0],nums[1]
-    // iterate twice with 2 vars.. => 
-    // for()
-    //   for() => 0n^2
-    // hashmap --> save the [complement, index] --> [(9-2), 0] -> [7, 0]
-    // [7, 0]
-    // [2, 1]
-    // [-2, 2]... 
+    // bf: iterate nums 2 times nad find a pair that sums up to target. => O(n^2)
+    // optimized: Use a hashmap
+    // value    : index.  [2, 7, 11, 15] target = 9 
+    //   7 (9-2):  0       0, 1,  2, 3
+    // move to next (7), that is in the hashmap.
+    // return [0(current number's hashmap index), 1 (current number index)]
 
     let map = new Map();
-    
-    for(let i=0; i<nums.length; i++){
-        let complememt = target - nums[i];
-        if(map.has(nums[i])){
-            return [map.get(nums[i]), i]
-        }else{
-        map.set(complememt, i);
-        }
 
+    for(let i=0; i<nums.length; i++){
+
+        if(map.has(nums[i])){
+            return [map.get(nums[i]), i];
+        }else{
+            let complement = target - nums[i];
+            map.set(complement, i);
+        }
     }
+
     return [];
 };
